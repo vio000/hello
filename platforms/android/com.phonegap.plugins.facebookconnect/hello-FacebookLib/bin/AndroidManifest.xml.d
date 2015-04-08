edbea317fafb0941c67b46e633c3280cfd1f7166
vio000 <vio000@yahoo.com>
2015-04-09 02:39:21 +0300

    fb

---
+++ platforms/android/com.phonegap.plugins.facebookconnect/hello-FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\Projects\GitHub\hello\platforms\android\com.phonegap.plugins.facebookconnect\hello-FacebookLib\ant-build\AndroidManifest.xml : \
+D:\Projects\GitHub\hello\platforms\android\com.phonegap.plugins.facebookconnect\hello-FacebookLib\AndroidManifest.xml \
